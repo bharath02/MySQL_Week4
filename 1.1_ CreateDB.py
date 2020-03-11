38cba1c461a7f115da0196f76d8717f2f30763ae
bharath02 <fee0228@gmail.com>
2020-03-11 17:27:16 +0530

    create table variable names

--- 1.1_ CreateDB.py
+++ 1.1_ CreateDB.py
@@ -1,4 +1,7 @@
 import mysql.connector
 mydb=mysql.connector.connect(host="localhost",user='root',password='password',database='DB1')
 mybase=mydb.cursor()
-mybase.execute("Create table employee(firstName varchar(50), lastName varchar(50),age int(10),salary int(20))")
+#mybase.execute("Create table employee(firstName varchar(50), lastName varchar(50),age int(10),salary int(20))")
+mybase.execute("Show tables")
+for table in mybase:
+    print(table)
