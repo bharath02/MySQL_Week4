f1c9f6d92f29149bdf11e11625f75287b271493c
bharath02 <fee0228@gmail.com>
2020-03-15 13:32:33 +0530

    show the Tables in DataBase

--- 2.0_showDataBASE.py
+++ 2.0_showDataBASE.py
@@ -2,5 +2,5 @@ import mysql.connector
 mydb=mysql.connector.connect(host="localhost",user='root',password='password')
 mybase=mydb.cursor()
 mybase.execute("Show databases")
-for bata in mybase:
-    print(bata)
+for data in mybase:
+    print(data)
